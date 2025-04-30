393d9f028a6c56ad834b836b32141d3be4572979
Angel Absé <anabhi01@gmail.com>
2025-04-30 00:36:42 -0400

    se agrega adaptador vercel para generar contenido dinámico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 import tailwind from '@astrojs/tailwind';
@@ -7,4 +8,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
